286d3709e835ba5d59fcd434933513205b894e17
Draqneel <marabucavi@gmail.com>
2017-10-04 18:43:49 +0300

    Must working, but not

---
+++ Homeworks/Task4/src/com/company/ArrayList.java
@@ -0,0 +1,179 @@
+package com.company;
+
+public class ArrayList {
+    // константа - в коде уже изменить не получится
+    private final int INITIAL_SIZE = 100;
+    // поле - хранилище данных
+    private int elements[];
+    // количество элементов массива
+    private int count;
+    // для сортировки выбором
+    private int temp;
+    private int imin;
+    private int min;
+
+    public ArrayList() {
+        elements = new int[INITIAL_SIZE];
+        count = 0;
+    }
+
+    /**
+     * Добавить элемент в конец
+     * switch 2.1
+     *
+     * @param element
+     */
+    public void add(int element) {
+        if (count < elements.length) {
+            elements[count] = element;
+            count++;
+        } else {
+            // TODO: увеличение размера массива
+            System.err.println("Нет места");
+        }
+    }
+
+    /**
+     * Добавить элемент в начало списка, со сдвигом вправо
+     * switch 2.2
+     *
+     * @param element
+     */
+    public void addToBegin(int element) {
+        if (count < elements.length) {
+            for (int i = count; i > 0; i--) {
+                elements[i - 1] = elements[i];
+            }
+            elements[0] = element;
+            count++;
+        } else {
+            System.out.println("Нет места");
+        }
+    }
+
+
+    /**
+     * Вставить элемент в заданную позицию со сдвигом
+     * switch 2.3
+     *
+     * @param element сам элемент
+     * @param index   индекс, куда надо вставить
+     */
+    public void insert(int element, int index) {
+        if (index >= 0 && index < count) {
+            if (index == count - 1) {
+                elements[count - 1] = element;
+            } else {
+                for (int i = count; i > index; i--) {
+                    elements[count] = elements[count - 1];
+                }
+                elements[index] = element;
+            }
+            count++;
+
+        } else {
+            System.out.println("Пусткой индекс");
+        }
+
+    }
+
+    /**
+     * Получить элемент по индексу
+     * switch 2.4
+     *
+     * @param index
+     * @return сам элемент по заданному индексу
+     */
+    public int get(int index) {
+        if (elements[index] != 0) {
+            return elements[index];
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * Заменить элемент в заданной позиции новым элементом
+     * switch 2.5
+     *
+     * @param index
+     * @param element
+     */
+    public void replace(int index, int element) {
+        if (elements[index] != 0) {
+            elements[index] = element;
+        } else {
+            System.out.println("Пуской индекс");
+        }
+    }
+
+    // switch 2.6
+    public void sort() {
+        for (int i = 0; i < count; i++) {
+            min = elements[i];
+            imin = i;
+            for (int j = i + 1; j < count; j++) {
+                if (elements[j] < min) {
+                    imin = j;
+                }
+            }
+            if (i != imin) {
+                temp = elements[i];
+                elements[i] = elements[imin];
+                elements[imin] = temp;
+                temp = 0;
+            }
+        }
+    }
+
+    //switch 2.7
+    public void reverse() {
+        for (int i = 0; i < count % 2; i++) {
+            temp = elements[i];
+            elements[i] = elements[count - i - 1];
+            elements[count - i - 1] = temp;
+        }
+        temp = 0;
+
+    }
+
+    //switch 2.8
+    public void remove(int index) {
+        if (index >= 0 && index < count) {
+            if (index == count - 1) {
+                elements[count - 1] = 0;
+            } else {
+                for (int i = index; i < count; i++) {
+                    elements[i] = elements[i + 1];
+                }
+                elements[count - 1] = 0;
+            }
+            count--;
+        } else {
+            System.out.println("Пусткой индекс");
+        }
+
+    }
+
+    /**
+     * Вернуть индекс элемента, если элемента нет = -1
+     * switch 2.9
+     *
+     * @param element
+     * @return
+     */
+    public int find(int element) {
+        for (int i = 0; i < count; i++) {
+            if (elements[i] == element) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //удобство вывода
+    public int getElements(int index) {
+        return elements[index];
+    }
+
+}
---
+++ Homeworks/Task4/src/com/company/Main.java
@@ -0,0 +1,141 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        ArrayList lists[] = new ArrayList[5];
+        boolean a[] = new boolean[5];
+        // МЕНЮ
+        // 1. Создать список (0..4) - создаете нужный список
+        // 2. Показть все списки (показываются только ненулевые)
+        // 3. Работа со списком - попросить ввести номер спика
+        //       1. Добавить
+        //       2. Удалить
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Меню:");
+            System.out.println("1. Создать список");
+            System.out.println("2. Показать все списки");
+            System.out.println("3. Работа со списком");
+            int command = scanner.nextInt();
+            int listNumber;
+            int elementMenu;
+            int indexMenu;
+            switch (command) {
+                case 1:
+                    System.out.println("Введите номер создаваемого списка: [0..4]");
+                    listNumber = scanner.nextInt();
+                    if (listNumber > 4 && listNumber < 0) {
+                        System.out.println("Uncorrect number;");
+                        break;
+                    } else {
+                        lists[listNumber] = new ArrayList();
+                        a[listNumber] = true;
+                        break;
+                    }
+                case 2:
+                    for (int i = 0; i < lists.length; i++) {
+                        if (a[i] == true) {
+                            for (int j = 0; j < 100; j++) {
+                                System.out.print(lists[i].getElements(j) + " ");
+                                if (j % 10 == 9) {
+                                    System.out.println();
+                                }
+
+                            }
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 3:
+                    System.out.println("Выберите лист с которым хотите работать [0..4]");
+                    listNumber = scanner.nextInt();
+                    if (listNumber > 4 || listNumber < 0) {
+                        break;
+                    } else {
+                        System.out.println("Меню(2):");
+                        System.out.println("1) Добавить элемент в конец");
+                        System.out.println("2) Добавить элемент в начало");
+                        System.out.println("3) Вставить элемент в заданную позицию");
+                        System.out.println("4) Получить элемент по идексу");
+                        System.out.println("5) Заменить элемент в заданной позиции");
+                        System.out.println("6) Отсортировать список");
+                        System.out.println("7) Развернуть список");
+                        System.out.println("8) Удалить элемент по индексу");
+                        System.out.println("9) Вернуть интекс элемента");
+                        System.out.println();
+                        // работаем с меню 2.1
+                        switch (listNumber) {
+                            case 1:
+                                System.out.println("Введите элемент который хотите добавить в конец");
+                                elementMenu = scanner.nextInt();
+                                lists[listNumber].add(elementMenu);
+                                break;
+                            case 2:
+                                System.out.println("Введите элемент который хотите добавить в начало");
+                                elementMenu = scanner.nextInt();
+                                lists[listNumber].addToBegin(elementMenu);
+                                break;
+                            case 3:
+                                System.out.println("Введите элемент который хотите добавить ");
+                                elementMenu = scanner.nextInt();
+                                System.out.println("Введите позицию данного элемента ");
+                                indexMenu = scanner.nextInt();
+                                lists[listNumber].insert(elementMenu, indexMenu);
+                                break;
+                            case 4:
+                                System.out.println("Введите индекс искомого элемента");
+                                indexMenu = scanner.nextInt();
+                                if (lists[listNumber].get(indexMenu) == -1) {
+                                    System.out.println("Элемент отсутствует");
+                                } else {
+                                    System.out.println(lists[listNumber].get(indexMenu));
+                                }
+                                break;
+                            case 5:
+                                System.out.println("Ввежите позицию на которой вы бы хотели заменить элемент");
+                                indexMenu = scanner.nextInt();
+                                System.out.println("Введите новый элемент");
+                                elementMenu = scanner.nextInt();
+                                lists[listNumber].replace(indexMenu, elementMenu);
+                                break;
+                            case 6:
+                                lists[listNumber].sort();
+                                break;
+                            case 7:
+                                lists[listNumber].reverse();
+                                break;
+                            case 8:
+                                System.out.println("Элемент пож каким индексом вы хотели бы удалить?");
+                                indexMenu = scanner.nextInt();
+                                lists[listNumber].remove(indexMenu);
+                                break;
+                            case 9:
+                                System.out.println("Введите элемент, индекс которого нужно найти");
+                                elementMenu = scanner.nextInt();
+                                if (lists[listNumber].find(elementMenu) == -1) {
+                                    System.out.println("Такого элемента нет");
+                                } else {
+                                    System.out.println(lists[listNumber].find(elementMenu));
+                                }
+                                break;
+                            default:
+                                System.out.println("Команда не найдена");
+                                break;
+                        }
+
+
+                    }
+
+
+                    break;
+                default:
+                    System.out.println("ERROR : Неверное значение");
+                    break;
+            }
+        }
+    }
+}
